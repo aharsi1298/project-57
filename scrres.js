d97a758a3a58e8fd1777af58fea146936a0af370
aharsi1298 <82213601+aharsi1298@users.noreply.github.com>
2021-05-08 11:54:31 +0530

    Delete scrres.js

--- scrres.js
+++
@@ -1,9 +0,0 @@
-var width = window.screen.width;
-var height = window.screen.height;
-
-document.getElementById("welcome").style.width = width;
-document.getElementById("welcome").style.height = height;
-
-document.getElementsByClassName("link")[1].addEventListener("click", function(e) {
-	location.href = location.href - "#videos"
-})
